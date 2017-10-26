3e088ed16d1065b28a31da5c07aa338bcc91031a
NinnYu Chin <nychin92@gmail.com>
2017-10-25 22:41:07 -0400

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -1,13 +1,13 @@
 /* 	Homework 2, Question 1
- 	@author NinnYu
- 	Description: 	Write a program that asks the user how tall of a pyramid do they want to 
-			display on the screen, and then displays the pyramid that counts up on the 
-			left, and counts down on the right. For eg, if the user enters 4, the 
-			program shows:
-			        1
-			      1 2 1
-			    1 2 3 2 1
-			  1 2 3 4 3 2 1
+ *	@author NinnYu
+ *	Description: 	Write a program that asks the user how tall of a pyramid do they want to 
+ *			display on the screen, and then displays the pyramid that counts up on the 
+ *			left, and counts down on the right. For eg, if the user enters 4, the 
+ *			program shows:
+ *			        1
+ *			      1 2 1
+ *			    1 2 3 2 1
+ *			  1 2 3 4 3 2 1
  */
 
 import java.util.*;
