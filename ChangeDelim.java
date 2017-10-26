93a54e908dd67866efc9b667d20cab819a17a68e
NinnYu Chin <nychin92@gmail.com>
2017-10-25 22:39:42 -0400

    Update ChangeDelim.java

--- ChangeDelim.java
+++ ChangeDelim.java
@@ -1,8 +1,8 @@
 /* 	Homework 2, Question 5
- 	@author NinnYu Chin
- 	Instruction:	Change Scanner's delimiter, and tokenize comma separated values entered by 
-			the keyboard.
- 	Reference: https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
+ *	@author NinnYu Chin
+ *	Instruction:	Change Scanner's delimiter, and tokenize comma separated values entered by 
+ *			the keyboard.
+ *	Reference: https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
  */
 
 import java.util.*;
