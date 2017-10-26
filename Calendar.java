a5e99a24b2c62fc37a0be3af8313fcac8c1dbd5c
NinnYu Chin <nychin92@gmail.com>
2017-10-25 22:39:20 -0400

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -1,15 +1,15 @@
 /* 	Homework 2, Question 2
- 	@author NinnYu Chin
- 	Instruction:	Write a program that displays a calendar on the screen. It asks the user 
-			the year, what day the first of January fell on, then displays every month 
-			from January to December. Make sure to get the leap years right. Your 
-			months should resemble the following format:
-			   February 2015      
-			Su Mo Tu We Th Fr Sa  
-			 1  2  3  4  5  6  7  
-			 8  9 10 11 12 13 14  
-			15 16 17 18 19 20 21  
-			22 23 24 25 26 27 28 
+ *	@author NinnYu Chin
+ *	Instruction:	Write a program that displays a calendar on the screen. It asks the user 
+ *			the year, what day the first of January fell on, then displays every month 
+ *			from January to December. Make sure to get the leap years right. Your 
+ *			months should resemble the following format:
+ *			   February 2015      
+ *			Su Mo Tu We Th Fr Sa  
+ *			 1  2  3  4  5  6  7  
+ *			 8  9 10 11 12 13 14  
+ *			15 16 17 18 19 20 21  
+ *			22 23 24 25 26 27 28 
  */
 
 
